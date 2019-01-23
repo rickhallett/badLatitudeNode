c2b164a3f987c820a7174df1ced25a371263af7b
rickhallett <richard.hallett@ims-evolve.com>
2019-01-23 13:27:37 +0000

    prototype google map api geocode script

--- parser.js
+++ parser.js
@@ -1,5 +1,6 @@
 const xlsx = require('xlsx');
 const chalk = require('chalk');
+const keys = require('./keys');
 
 const config = {
   ADDRESS_COLUMN: 'A',
@@ -23,6 +24,48 @@ while(!end_of_column) {
     }
 }
 
+const googleMapsClient = require('@google/maps').createClient({
+    key: keys.googlemaps,
+    Promise: Promise
+});
+
+const processRawJSON = (data) => {
+    const { formatted_address: addr } = data;
+    const { lat, lng } = data.geometry.location;
+
+    return {
+        addr: addr,
+        lat: lat,
+        lng: lng
+    }
+};
+
+async function fetchOne(address){
+    const res = await googleMapsClient.geocode({ address: address }).asPromise();
+
+    if(!res) {
+        return null;
+    } else {
+        return res.json.results[0];
+    }
+}
+
+
+async function fetchAll(array) {
+    let coords = [];
+    for(const address of array) {
+        coords.push(processRawJSON(await fetchOne(address)));
+    }
+
+    return coords;
+}
+
+const home = '16 Streche road Swanage Dorset BH19 1NF';
+const work = 'Matrix House North Fourth Street Milton Keynes MK9 1AY';
+const dr = 'Swanage Medical Practice';
+const testAddrArray = [home, work, dr];
+
+fetchAll(testAddrArray).then(res => console.table(res));
 
 
 const breakpoint = 'this is a breakpoint.';
