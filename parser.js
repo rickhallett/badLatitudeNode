211bdbe7af7b7d17478c78f53e282f9680834337
rickhallett <richard.hallett@ims-evolve.com>
2019-01-24 09:38:32 +0000

    add error handling try/catch

--- parser.js
+++ parser.js
@@ -27,26 +27,42 @@ const googleMapsClient = require('@google/maps').createClient({
 });
 
 function processRawJSON(data){
-    log('processing raw JSON');
-    const { formatted_address: addr } = data;
-    const { lat, lng } = data.geometry.location;
 
-    return {
-        addr: addr,
-        lat: lat,
-        lng: lng
+    try {
+        log('processing raw JSON');
+        const { formatted_address: addr } = data;
+        const { lat, lng } = data.geometry.location;
+
+        return {
+            addr: addr,
+            lat: lat,
+            lng: lng
+        }
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError processing Google Maps Geocode returned JSON\n'));
+        console.log(chalk.red(err.stack));
     }
+
+
 }
 
 async function fetchOne(address){
-    async_log('Fetch address from Google API');
-    const res = await googleMapsClient.geocode({ address: address }).asPromise();
 
-    if(!res) {
-        return null;
-    } else {
-        return res.json.results[0];
+    try {
+        async_log('Fetch address from Google API');
+        const res = await googleMapsClient.geocode({ address: address }).asPromise();
+
+        if(!res) {
+            return null;
+        } else {
+            return res.json.results[0];
+        }
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError fetching address from Google Geocode API\n'));
+        console.log(chalk.red(err.stack));
     }
+
+
 }
 
 async function fetchAll(array) {
@@ -65,111 +81,130 @@ function getWorkbook(filename) {
 }
 
 function getWorksheet(filename) {
-    log('Reading in worksheet');
-    const file = xlsx.readFile(filename);
-    const firstSheetName = file.SheetNames[0];
-    return file.Sheets[firstSheetName];
+
+    try {
+        log('Reading in worksheet');
+        const file = xlsx.readFile(filename);
+        const firstSheetName = file.SheetNames[0];
+        return file.Sheets[firstSheetName];
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError reading in worksheet\n'));
+        console.log(chalk.red(err.stack));
+    }
+
+
 }
 
 function getAddresses(worksheet) {
-    log('Processing address column into memory');
-    let end_of_column = false;
-    let column = config.READ_ADDRESS_COLUMN;
-    let row = config.READ_ADDRESS_ROW_START;
-    // remove first element (column title)
-    delete worksheet[`${column}${row}`];
-    row++;
-    let addresses = [];
-    while(!end_of_column) {
-        const cell = `${column}${row}`;
-        if(worksheet[cell]) {
-            addresses.push(worksheet[cell].h);
-            row++;
-        } else {
-            end_of_column = true;
+
+    try {
+        log('Processing address column into memory');
+        let end_of_column = false;
+        let column = config.READ_ADDRESS_COLUMN;
+        let row = config.READ_ADDRESS_ROW_START;
+        // remove first element (column title)
+        delete worksheet[`${column}${row}`];
+        row++;
+        let addresses = [];
+        while(!end_of_column) {
+            const cell = `${column}${row}`;
+            if(worksheet[cell]) {
+                addresses.push(worksheet[cell].h);
+                row++;
+            } else {
+                end_of_column = true;
+            }
         }
+        return addresses;
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError processing new address columns\n'));
+        console.log(chalk.red(err.stack));
     }
-    return addresses;
+
+
 }
 
 function mergeResults(results, worksheet) {
-    log('Merge results into worksheet');
-    let lat_column = config.WRITE_ADDRESS_COLUMN_LATITUDE;
-    let long_column = config.WRITE_ADDRESS_COLUMN_LONGITUDE;
-    let row = config.WRITE_ADDRESS_ROW_START;
+    try {
+        log('Merge results into worksheet');
+        let lat_column = config.WRITE_ADDRESS_COLUMN_LATITUDE;
+        let long_column = config.WRITE_ADDRESS_COLUMN_LONGITUDE;
+        let row = config.WRITE_ADDRESS_ROW_START;
 
-    worksheet[`${lat_column}${row}`] = { v: 'Latitude' };
-    worksheet[`${long_column}${row}`] = { v: 'Longitude' };
-    row++;
-
-    results.forEach((el) => {
-        worksheet[`${lat_column}${row}`] = { v: el.lat };
-        worksheet[`${long_column}${row}`] = { v: el.lng };
+        worksheet[`${lat_column}${row}`] = { v: 'Latitude' };
+        worksheet[`${long_column}${row}`] = { v: 'Longitude' };
         row++;
-    });
 
-    config.areaRef = `A1:${long_column}${row - 1}`
+        results.forEach((el) => {
+            worksheet[`${lat_column}${row}`] = { v: el.lat };
+            worksheet[`${long_column}${row}`] = { v: el.lng };
+            row++;
+        });
+
+        config.areaRef = `A1:${long_column}${row - 1}`
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError merging results into worksheet\n'));
+        console.log(chalk.red(err.stack));
+    }
 }
 
 function writeToNewFile(worksheet, nextSheetName) {
-    log(`Writing to new file: processed-${nextSheetName}`);
-    worksheet['!ref'] = config.areaRef;
-    worksheet['!cols'] = [{ wpx: 150 }, { wpx: 95 }, { wpx: 95 }];
-    let newWorkBook = xlsx.utils.book_new();
-    xlsx.utils.book_append_sheet(newWorkBook, worksheet, 'Processed Sheet1');
-    xlsx.writeFile(newWorkBook, `processed-${nextSheetName}`);
+    try {
+        log(`Writing to new file: processed-${nextSheetName}`);
+        worksheet['!ref'] = config.areaRef;
+        worksheet['!cols'] = [{ wpx: 150 }, { wpx: 95 }, { wpx: 95 }];
+        let newWorkBook = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(newWorkBook, worksheet, 'Processed Sheet1');
+        xlsx.writeFile(newWorkBook, `processed-${nextSheetName}`);
+    } catch (err) {
+        console.log(chalk.yellow.underline.bgRed('\nError writing to file\n'));
+        console.log(chalk.red(err.stack));
+    }
 }
 
 
 
 // Main
 
-// if(cluster.isMaster) {
+fs.readdir(__dirname, function (err, items) {
 
-    // Use multi-threading for potential speed improvements
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
+    if(err) {
+        console.log(chalk.yellow.underline.bgRed('\nError reading from files:\n'));
+        console.table(items);
+        return;
+    }
 
-// } else {
+    const excelSheets = [];
+    const isExcelSheet = (filename) => filename.match(/(\w)+(.xlsx)/);
+    const isNotProcessed = (filename) => !filename.match(/(processed-)/);
 
-
-    fs.readdir(__dirname, function (err, items) {
-
-        const excelSheets = [];
-        const isExcelSheet = (filename) => filename.match(/(\w)+(.xlsx)/);
-        const isNotProcessed = (filename) => !filename.match(/(processed-)/);
-
-        items.forEach(filename => {
-            if (isExcelSheet(filename) && isNotProcessed(filename)) {
-                excelSheets.push(filename);
-            }
-        });
-
-        async function asyncRecurse(nextSheet) {
-            if (!nextSheet) {
-                return false;
-            } else {
-                const worksheet = getWorksheet(nextSheet);
-                fetchAll(getAddresses(worksheet)).then(coords => {
-                    mergeResults(coords, worksheet);
-                    writeToNewFile(worksheet, nextSheet);
-
-                    return asyncRecurse(excelSheets.pop());
-                }).catch(err => {
-                    console.log(err.message);
-                });
-            }
-
+    items.forEach(filename => {
+        if (isExcelSheet(filename) && isNotProcessed(filename)) {
+            excelSheets.push(filename);
         }
-
-        asyncRecurse(excelSheets.pop());
-
     });
 
+    async function asyncRecurse(nextSheet) {
+        if (!nextSheet) {
+            return false;
+        } else {
+            const worksheet = getWorksheet(nextSheet);
+            fetchAll(getAddresses(worksheet)).then(coords => {
+                mergeResults(coords, worksheet);
+                writeToNewFile(worksheet, nextSheet);
 
-// }
+                return asyncRecurse(excelSheets.pop());
+            }).catch(err => {
+                console.log(err.message);
+            });
+        }
+
+    }
+
+    asyncRecurse(excelSheets.pop());
+
+});
+
 
 
 
